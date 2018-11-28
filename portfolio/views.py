b9390dfc0132a1ab93fd9e7bac8981640de726bb
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-11-28 04:02:53 -0600

    Adds success message to contactview, once email successfully delivered a success message will pop up

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from portfolio.forms import ContactForm
 from django.views.generic.edit import FormView
+from django.contrib.messages.views import SuccessMessageMixin
+
 # Create your views here.
 
 
@@ -12,10 +14,11 @@ def home(request):
     return render(request, 'portfolio/home.html')
 
 
-class ContactView(FormView):
+class ContactView(SuccessMessageMixin, FormView):
     template_name = 'contact.html'
     form_class = ContactForm
     success_url = '/contact/'
+    success_message = "Thank you %(name)s ! Your message has been sent!"
 
     def form_valid(self, form):
 
